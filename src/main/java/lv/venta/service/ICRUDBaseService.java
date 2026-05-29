cf2c11db203a193530a946be65082a4050665bcb
KarinaSkirmante <karina.krinkele@gmail.com>
2026-05-29 17:33:14 +0300

    Add: added CRUD service interface for Student and fix create int he CRUD Base

--- src/main/java/lv/venta/service/ICRUDBaseService.java
+++ src/main/java/lv/venta/service/ICRUDBaseService.java
@@ -3,8 +3,10 @@ package lv.venta.service;
 import java.util.ArrayList;
 
 public interface ICRUDBaseService<Ttype> {
-	//retrieve all, retrieve by id un delete by id tiks definēts šeit, jo tiem var izmantot Ttype
+	//create, retrieve all, retrieve by id un delete by id tiks definēts šeit, jo tiem var izmantot Ttype
 
+	public abstract void create(Ttype newObject) throws Exception;  
+	
 	public abstract ArrayList<Ttype> retrieveAll() throws Exception;
 	
 	public abstract Ttype retrieveById(long id) throws Exception;
