fb3434a80b6114ee6d7e36288e635d2d5ad91f40
KarinaSkirmante <karina.krinkele@gmail.com>
2026-05-29 17:25:47 +0300

    Add: added CRUDBASE interface for service

---
+++ src/main/java/lv/venta/service/ICRUDBaseService.java
@@ -0,0 +1,13 @@
+package lv.venta.service;
+
+import java.util.ArrayList;
+
+public interface ICRUDBaseService<Ttype> {
+	//retrieve all, retrieve by id un delete by id tiks definēts šeit, jo tiem var izmantot Ttype
+
+	public abstract ArrayList<Ttype> retrieveAll() throws Exception;
+	
+	public abstract Ttype retrieveById(long id) throws Exception;
+	
+	public abstract void deleteById(long id) throws Exception;
+}
