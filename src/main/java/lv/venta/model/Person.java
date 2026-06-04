735823545087262220811d262b3114a9a3ebf312
KarinaSkirmante <karina.krinkele@gmail.com>
2026-06-04 13:46:59 +0300

    Edit: changed to @inheritance in the super class with strategy TABLE_PER_CLASS

--- src/main/java/lv/venta/model/Person.java
+++ src/main/java/lv/venta/model/Person.java
@@ -1,26 +1,41 @@
 package lv.venta.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-//piemērs, kad mantosana ir tikai java līmenī, līdz ar to nebūs ne @table, ne @Entity
+//piemērs, kad mantosana ir arī DB līmenī
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor//visu argumentu konstruktors, jo abi mainīgie būs jāauzstāda caur konstruktoru
 @ToString
-@MappedSuperclass//jo taisam kā super klasi, kuru mantos entītijas klases
+@Table(name = "PersonTable")
+@Entity
+//strategy = InheritanceType.TABLE_PER_CLASS <-- no katras klases, kura ir iesiastīa mantosanā, būs tabula 
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Person {
 	
-	//id nevajag, jo sī klases nebūs tabula DB
+	//ja bāzes klase ir @table, tad obligāti jaieliek id
+	@Column(name = "Idp")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idp;
 	
 	@Column(name = "Name")
 	@NotNull
@@ -34,4 +49,8 @@ public class Person {
 	@Pattern(regexp = "[A-Ž]{1}[a-ž]{2,20}([ -]{1}([A-Ž]{1}[a-ž]{2,20}))?")
 	private String surname;
 
+	public Person(String name, String surname) {
+		setName(name);
+		setSurname(surname);
+	}
 }
